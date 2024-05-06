9c0e95a90382168db41af938cb04e8091265a0bc
IT21223976 <it21223976@my.sliit.lk>
2024-05-06 16:19:06 +0530

    Home page

--- paf-frontend/src/components/NewsFeed.js
+++ paf-frontend/src/components/NewsFeed.js
@@ -1,50 +1,56 @@
 import React, { useEffect } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { Button, Col, Container, Nav, Navbar, Row } from "react-bootstrap";
-import logo from "./assets/psn-logo-large.jpeg";
+import logo from "./assets/psn-Logo-large.jpg";
 
-import {
-  RiNewspaperLine,
-  RiRadarLine,
-  RiBaseStationLine,
-  RiFolderUserLine,
-  RiLogoutBoxLine,
-  RiUserAddLine,
-} from "react-icons/ri";
+import
+  {
+    RiNewspaperLine,
+    RiRadarLine,
+    RiBaseStationLine,
+    RiFolderUserLine,
+    RiLogoutBoxLine,
+    RiUserAddLine,
+  } from "react-icons/ri";
 
 //import { BsPersonFillAdd } from "react-icons/bs";
 
 import styles from "./styles/NewsFeed.module.css";
 
-function NewsFeed() {
+function NewsFeed ()
+{
   let navigate = useNavigate();
 
-  function handleClick(e) {
-    navigate("/newsfeed/allaccounts");
+  function handleClick ( e )
+  {
+    navigate( "/newsfeed/allaccounts" );
   }
 
-  function handleSignOut(e) {
-    localStorage.removeItem("psnUserId");
-    localStorage.removeItem("psnToken");
-    localStorage.removeItem("psnUserFirstName");
-    localStorage.removeItem("psnUserLastName");
-    localStorage.removeItem("psnUserEmail");
-    navigate("/");
+  function handleSignOut ( e )
+  {
+    localStorage.removeItem( "psnUserId" );
+    localStorage.removeItem( "psnToken" );
+    localStorage.removeItem( "psnUserFirstName" );
+    localStorage.removeItem( "psnUserLastName" );
+    localStorage.removeItem( "psnUserEmail" );
+    navigate( "/" );
   }
 
-  useEffect(() => {
-    if (localStorage.getItem("psnToken") === null) {
-      navigate("/unauthorized");
+  useEffect( () =>
+  {
+    if ( localStorage.getItem( "psnToken" ) === null )
+    {
+      navigate( "/unauthorized" );
     }
-  });
+  } );
 
   return (
     <Container className="pt-3">
       <Row className="mb-3">
-        <Col md={4}>
+        <Col md={ 4 }>
           <Row className="justify-content-center align-items-center">
             <Col md="auto" className="text-sm-start text-center mb-sm-0 mb-3">
-              <img src={logo} width="125" alt="logo" />
+              <img src={ logo } width="125" alt="logo" />
             </Col>
             <Col className="text-sm-start text-center text-success mb-sm-0 mb-3">
               <h1>GIM</h1>
@@ -60,18 +66,18 @@ function NewsFeed() {
         </Col> */}
       </Row>
       <Row>
-        <Col md={4}>
+        <Col md={ 4 }>
           <Navbar bg="light" expand="lg" className="mb-3 mb-sm-0">
-            <Container className={styles.navbarContainer}>
+            <Container className={ styles.navbarContainer }>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse>
-                <Nav className={styles.navContainer}>
+                <Nav className={ styles.navContainer }>
                   <ul className="list-group">
                     <Nav.Link>
                       <Link to="" className="text-decoration-none">
                         <li className="list-group-item fs-5 py-3 text-success shadow">
                           <span>
-                            {" "}
+                            { " " }
                             <RiNewspaperLine /> Newsfeed
                           </span>
                         </li>
@@ -97,8 +103,8 @@ function NewsFeed() {
                     </Nav.Link>
                     <Nav.Link>
                       <li
-                        className={`list-group-item fs-5 py-3 text-success shadow ${styles.signOutButton}`}
-                        onClick={handleClick}
+                        className={ `list-group-item fs-5 py-3 text-success shadow ${ styles.signOutButton }` }
+                        onClick={ handleClick }
                       >
                         <span>
                           <RiUserAddLine /> Add Friends
@@ -134,8 +140,8 @@ function NewsFeed() {
                     </Nav.Link>
                     <Nav.Link>
                       <li
-                        className={`list-group-item fs-5 py-3 text-success shadow ${styles.signOutButton}`}
-                        onClick={handleSignOut}
+                        className={ `list-group-item fs-5 py-3 text-success shadow ${ styles.signOutButton }` }
+                        onClick={ handleSignOut }
                       >
                         <span>
                           <RiLogoutBoxLine /> Sign Out
@@ -148,8 +154,8 @@ function NewsFeed() {
             </Container>
           </Navbar>
         </Col>
-        <Col md={8}>
-          <Outlet />{" "}
+        <Col md={ 8 }>
+          <Outlet />{ " " }
         </Col>
       </Row>
     </Container>
