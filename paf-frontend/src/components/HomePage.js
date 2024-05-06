1d923e2c9d6c15d047095b4fee599b62bd7ab813
IT21223976 <it21223976@my.sliit.lk>
2024-05-06 15:53:12 +0530

    Home Page

--- paf-frontend/src/components/HomePage.js
+++ paf-frontend/src/components/HomePage.js
@@ -6,13 +6,14 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
-import {
-  BsFillBookFill,
-  BsGithub,
-  BsFillShareFill,
-  BsFillPersonPlusFill,
-  BsFillCpuFill,
-} from "react-icons/bs";
+import
+  {
+    BsFillBookFill,
+    BsGithub,
+    BsFillShareFill,
+    BsFillPersonPlusFill,
+    BsFillCpuFill,
+  } from "react-icons/bs";
 
 import { RiLoginBoxLine } from "react-icons/ri";
 
@@ -20,23 +21,26 @@ import styles from "./styles/HomePage.module.css";
 
 import psnLogo from "./assets/psn-logo-large.jpeg";
 
-function HomePage() {
+function HomePage ()
+{
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (localStorage.getItem("psnToken") !== null) {
-      navigate("/newsfeed");
+  useEffect( () =>
+  {
+    if ( localStorage.getItem( "psnToken" ) !== null )
+    {
+      navigate( "/newsfeed" );
     }
-  });
+  } );
 
   return (
     <Container fluid>
-      <Row className={styles.container}>
-        <Col className={`${styles.colContainerLeft} ${styles.leftBackground}`}>
+      <Row className={ styles.container }>
+        <Col className={ `${ styles.colContainerLeft } ${ styles.leftBackground }` }>
           <div>
             <Row>
               <h3 className="my-3">
-                <BsFillBookFill /> GainTrain
+                <BsFillBookFill /> Fitness
               </h3>
             </Row>
             {/* <Row>
@@ -56,9 +60,9 @@ function HomePage() {
             </Row> */}
           </div>
         </Col>
-        <Col className={styles.colContainerRight}>
-          <div className={styles.colWithButtons}>
-            <img src={psnLogo} alt="PSN logo" width={120} className="mb-3" />
+        <Col className={ styles.colContainerRight }>
+          <div className={ styles.colWithButtons }>
+            <img src={ psnLogo } alt="PSN logo" width={ 120 } className="mb-3" />
             <Row>
               <h1 className="text-success mb-3">
                 Fitness Community Building Connection Motivation
@@ -69,21 +73,21 @@ function HomePage() {
               <h3 className="text-success mb-3">
                 Join GainTrain social network today
               </h3>
-            </Row>{" "}
+            </Row>{ " " }
             <br />
             <Row>
-              <Link to="/signin" className={styles.linkTextFormat}>
+              <Link to="/signin" className={ styles.linkTextFormat }>
                 <Button
                   variant="success"
-                  className={`${styles.btnHomePage} mb-3`}
+                  className={ `${ styles.btnHomePage } mb-3` }
                 >
                   Sign In <RiLoginBoxLine />
                 </Button>
               </Link>
             </Row>
             <Row>
-              <Link to="/signup" className={styles.linkTextFormat}>
-                <Button variant="success" className={styles.btnHomePage}>
+              <Link to="/signup" className={ styles.linkTextFormat }>
+                <Button variant="success" className={ styles.btnHomePage }>
                   Sign Up <BsFillPersonPlusFill />
                 </Button>
               </Link>
